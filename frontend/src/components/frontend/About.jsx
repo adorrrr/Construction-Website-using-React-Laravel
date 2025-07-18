22b1614eed98587b576d6524a52cd2436157eaf6
Ahasanul Monir Adar <131952423+adorrrr@users.noreply.github.com>
2025-07-18 22:06:56 +0600

    18-07-25

--- frontend/src/components/frontend/About.jsx
+++ frontend/src/components/frontend/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../common/Header'
 import Footer from '../common/Footer'
+import {default as AboutNew} from '../common/About';
 
 const About = () => {
   return (
@@ -9,8 +10,8 @@ const About = () => {
       <main>
         <section className='section-7'>
           <div className='hero d-flex align-items-center'>
-              <div className='container-fluid'>
-                  <div className='ps-5'>
+              <div className='container'>
+                  <div className='text-left'>
                       <span>Quality. Integrity. Value.</span>
                       <h1>About Us</h1>
                       <p>We excel at transforming visions into reality <br /> through outstanding craftsmanship and precise.</p>
@@ -19,7 +20,21 @@ const About = () => {
               </div>
           </div>
         </section>
-        
+
+        <AboutNew/>
+
+
+        {/* Our Team */}
+        <section className='section-8 bg-light py-5'>
+          <div className='container'>
+            <div className='section-header text-center py-5'>
+                <div className='mb-2'><span>Why Choose Us</span></div>
+                <h2>Discover our diverse range of projects</h2>
+                <p>Created in close partnership with our clients and collaborators, this approach merges industry expertise,<br />decades of experience, innovation, and flexibility to consistently deliver excellence</p>
+              </div>
+          </div>
+        </section>
+
       </main>
       <Footer/>
     </>
--- frontend/src/components/frontend/Home.jsx
+++ frontend/src/components/frontend/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import AboutImg from '../../assets/images/about-us.jpg';
 import ServiceImg1 from '../../assets/images/construction1.jpg';
 import ServiceImg2 from '../../assets/images/construction2.jpg';
 import ServiceImg3 from '../../assets/images/construction3.jpg';
@@ -16,6 +15,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import {Pagination} from 'swiper/modules';
 import 'swiper/css/pagination';
+import About from '../common/About';
 
 
 
@@ -43,22 +43,8 @@ const Home = () => {
           </section>
 
           {/* About Us Section */}
-          <section className='section-2 py-5'>
-            <div className='container py-5'>
-              <div className='row'>
-                <div className='col-md-6'>
-                  <img src={AboutImg} className='w-100'/>
-                </div>
-                <div className='col-md-6'>
-                  <span>About Us</span>
-                  <h2>Crafting structures that <br /> last a lifetime</h2>
-                  <p>Building enduring structures requires a comprehensive approach that combines advanced materials, resilient design, routine maintenance, and sustainable practices. By drawing on historical insights and utilizing modern technology. <br /><br />
+          <About/>
 
-                  Designing structures that stand the test of time involves a seamless blend of cutting-edge materials, durable design, ongoing upkeep, and eco-friendly practices. By combining lessons                 from the past with the power of modern technology.</p>
-                </div>
-              </div>
-            </div>
-          </section>
 
           {/* Our Services */}
           <section className='scetion-3 bg-light py-5'>
