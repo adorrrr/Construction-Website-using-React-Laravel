02997931c92e9a19e46efafdfed36f54c8539306
Ahasanul Monir Adar <131952423+adorrrr@users.noreply.github.com>
2025-07-18 21:48:23 +0600

    18-07-27

--- frontend/src/components/frontend/About.jsx
+++ frontend/src/components/frontend/About.jsx
@@ -6,7 +6,21 @@ const About = () => {
   return (
     <>
       <Header/>
-      <main>Adout</main>
+      <main>
+        <section className='section-7'>
+          <div className='hero d-flex align-items-center'>
+              <div className='container-fluid'>
+                  <div className='ps-5'>
+                      <span>Quality. Integrity. Value.</span>
+                      <h1>About Us</h1>
+                      <p>We excel at transforming visions into reality <br /> through outstanding craftsmanship and precise.</p>
+
+                  </div>
+              </div>
+          </div>
+        </section>
+        
+      </main>
       <Footer/>
     </>
   )
